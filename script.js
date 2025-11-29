b07fcbf6c02e98ad86d6ccb485b0e66e1756f17e
DevThrive <devthrivepty@gmail.com>
2025-11-29 11:50:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ const mobileMenu = document.getElementById("mobileMenu");
 hamburger.addEventListener("click", () => {
     mobileMenu.classList.toggle("open");
 });
+document.querySelectorAll(".mobile-link, #requestProposalMobile")
+  .forEach(link => {
+    link.addEventListener("click", () => {
+      mobileMenu.classList.remove("open");
+    });
+});
 
   function showPage(id) {
     pages.forEach(p => p.classList.toggle('active', p.id === id));
